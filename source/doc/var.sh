659f7c0639d9f27c9b71829d0767b7b0186e9931
Kamanati <98698457+Kamanati@users.noreply.github.com>
2022-06-03 11:09:44 +0530

    Add files via upload

---
+++ source/doc/var.sh
@@ -0,0 +1,15 @@
+# HA_AN - HACKING TOOL
+#https://github.com/kamanati/f-mailer
+
+
+# Color list
+end="\033[0m"
+green="\033[1;32m"
+red="\033[0;31m"
+white="\033[1;37m"
+flicker="\033[5m"
+
+# API LIST TO MYS (Make Your Server)
+server1="https://server-faquetool.000webhostapp.com/resources/send.php"
+server2="https://server2-faque-tool.000webhostapp.com/resources/send.php"
+server3="https://faque-service-bydog3r.000webhostapp.com/resources/send.php"
